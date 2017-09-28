408b7c318466fabf770f6d95d2691aadbc367ed8
Mayra Cabrera <mcabrera1087@gmail.com>
2017-09-28 18:07:47 -0500

    Add kata for 28 sept

---
+++ mayra-cabrera/28-september-magned-particular-in-boxes.go
@@ -0,0 +1,17 @@
+//https://www.codewars.com/kata/magnet-particules-in-boxes
+
+package kata
+
+import (
+	"math"
+)
+
+func Doubles(maxk, maxn int) float64 {
+	var sum = 0.0
+	for k := 1; k <= maxk; k++ {
+		for n := 1; n <= maxn; n++ {
+			sum += 1.0 / (float64(k) * math.Pow(float64(n+1), float64(2*k)))
+		}
+	}
+	return sum
+}
